0ef91c419654f0b0de7cebb0ae9ca3bc088ce03d
Arye Eidelman <aryedov.e@gmail.com>
2019-08-08 18:55:58 -0400

    Done.

---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+	
+	render(){
+		return (
+			<main><h1>Login!</h1></main>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import Home from './Home.js'
+import Login from './Login.js'
+import Signup from './Signup.js'
+import Settings from './Settings.js'
+import Account from './Account.js'
+
+
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const activeLink = { background: 'darkblue' }
+
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink exact to="/"   style={link} activeStyle={activeLink}>Home</NavLink>
+    <NavLink to="/login"    style={link} activeStyle={activeLink}>Login</NavLink>
+    <NavLink to="/signup"   style={link} activeStyle={activeLink}>Signup</NavLink>
+    <NavLink to="/settings" style={link} activeStyle={activeLink}>Settings</NavLink>
+    <NavLink to="/account"  style={link} activeStyle={activeLink}>Account</NavLink>
+  </div>;
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/settings" component={Settings} />
+      <Route path="/account" component={Account} />
+    </Fragment>
+  </Router>,
   document.getElementById('root')
 );
